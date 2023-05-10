ce9e960c4ea2bb471dae32c9c29ec39187c51d45
navaneetha <navani91@gmail.com>
2023-05-10 12:42:34 +0100

    Print in Shell

--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,23 @@
+echo -e "\e[35minstalling nginx\e[0m"
 yum install nginx -y
+
+echo -e "\e[35mremoving old content\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[35mdownloading frontend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[35mextracting downloaded frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
+echo -e "\e[35mcopying nginx config for roboshop\e[0m"
 cp configs/nginx-roboshop.cpnf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[35menabling nginx\e[0m"
 systemctl enable nginx
+
+echo -e "\e[35mstarting nginx\e[0m"
 systemctl start nginx
 
 
