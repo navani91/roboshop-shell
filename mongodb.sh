e23e5e1e5b84f240c13e0548aadbc5d449d4c01c
navaneetha <navani91@gmail.com>
2023-05-11 13:58:37 +0100

    Roboshop-Shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,18 @@
 source common.sh
 
-print_head "Setup mongoDb Ripository"
+print_head "Setup MongoDB repository"
 cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
-print_head "Install mongoDB"
+print_head "Install MongoDB"
 yum install mongodb-org -y &>>${log_file}
 
-print_head "Update mongoDB Listen Address"
+print_head "Update MongoDB Listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
-print_head "Enable mongoDB"
+print_head "Enable MongoDB"
 systemctl enable mongod &>>${log_file}
 
-print_head "restart mongoDB Service"
-systemctl start mongod &>>${log_file}
+print_head "Start MongoDB Service"
+systemctl restart mongod &>>${log_file}
 
-# update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
+
