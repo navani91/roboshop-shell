f8175d17dfbf27466c3e6e7a46a4e1b589d944cf
navaneetha <navani91@gmail.com>
2023-05-11 09:00:09 +0100

    Roboshop-Shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,21 @@
+code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+
+print_head() {
+  echo -e"\e[35m$1\e[0m"
+}
+
+print_head"Setup mongoDb Ripository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+print_head"Install mongoDB"
 yum install mongodb-org -y
+
+print_head"Enable mongoDB"
 systemctl enable mongod
+
+print_head"Start mongoDB Service"
 systemctl start mongod
 
 # update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
