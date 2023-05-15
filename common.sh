110a814096561c1eb1cd0ffd6042591ed5826530
navaneetha <navani91@gmail.com>
2023-05-15 10:34:50 +0100

    Roboshop-Shell

--- common.sh
+++ common.sh
@@ -92,5 +92,14 @@ status_check $?
 
 java() {
 
+yum install maven -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+cd /app
+unzip /tmp/shipping.zip
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
 
 }
